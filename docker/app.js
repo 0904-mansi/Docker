9586d558ca11a1373b5091d920a9f830ef8159d1
Mansi Mishra <mansimishra510@gmail.com>
2021-12-28 22:37:54 +0530

    Update app.js

--- docker/app.js
+++ docker/app.js
@@ -11,3 +11,19 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+
+
+// other file
+// load the http module
+var http = require('http');
+
+// configure our HTTP server
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello node.com\n");
+});
+
+// listen on localhost:8000
+server.listen(8080);
+console.log("Server listening at http://127.0.0.1:8080/");
